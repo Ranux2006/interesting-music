289a1f15b30efe668b744e10065d445e86e7879a
Ranux2006 <75128647+Ranux2006@users.noreply.github.com>
2020-11-28 09:03:43 +0530

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,43 @@
 basic.forever(function () {
-	
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+})
+basic.forever(function () {
+    music.playMelody("C5 B A G F E D C ", 120)
+    music.playMelody("C5 G B A F A C5 B ", 120)
+    music.playMelody("A F E F D G E F ", 120)
+    music.playMelody("C D E F G A B C5 ", 120)
 })
